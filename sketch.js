2a9453eeffb363b07a10d06e4c5fcaead285a167
Helb1a <94700968+Helb1a@users.noreply.github.com>
2022-01-17 16:36:41 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,50,50);
+}
+
+function draw() 
+{
+  background(30);
+  box.velocityX = 3;
+  drawSprites()
+}
+
+
+
+
